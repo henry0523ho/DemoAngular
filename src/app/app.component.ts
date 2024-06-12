2f7af8e5befd579efda5cb40d48b86252c6b3fa6
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2024-06-12 13:04:09 +0800

    feefefefef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, NotfoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+// import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  // constructor(private modalService: NgbModal) {}
+
+  
+
+}
